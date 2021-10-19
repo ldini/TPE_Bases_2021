fb3ac8698daee06557873df7bdc4937d0d189bcc
Leandro <leandro.david.dini@gmail.com>
2021-10-19 19:13:47 -0300

    Restricciones_ejercicio_a

--- CONSULTAS.sql
+++ CONSULTAS.sql
@@ -56,8 +56,18 @@ WHERE id_persona IN (
                                                         AND intervalo BETWEEN 5 and 10
                                                         AND l.id_servicio = s.id_servicio)));
 
+/*RESTRICCIONES*/
+/*a*/
+/*Si una persona está inactiva debe tener establecida una fecha de baja, la cual se debe controlar que sea al
+menos 18 años posterior a la de nacimiento.*/
 
-
+ALTER TABLE Persona
+ADD CONSTRAINT pk_persona_inactiva
+CHECK ((activo=false
+    AND fecha_baja is not null
+    AND (extract(YEAR from fecha_baja)-extract(YEAR from fecha_nacimiento))>=18 )
+    OR (activo = true)
+    );
 
 
 
