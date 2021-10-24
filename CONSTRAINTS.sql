a552ea1c8a26f07fce6b55ab0e35b91ac637423e
Maxi Barraza <gbarraza@alumnos.exa.unicen.edu.ar>
2021-10-24 17:50:50 -0300

    Restriccion 2.b) Sin terminar

--- CONSTRAINTS.sql
+++ CONSTRAINTS.sql
@@ -10,3 +10,35 @@ CHECK ((activo=false
     OR (activo = true)
     );
 
+/***B***
+El importe de un comprobante debe coincidir con la suma de los importes de sus líneas
+(si las tuviera).*/
+
+--Esta reestriccion es entre tablas, por lo tanto no se puede resolver de forma declarativa.
+
+--Forma declarativa(lo pide el enunciado):
+/*
+ALTER TABLE comprobante CHECK(
+    NOT EXISTS( SELECT 1
+                FROM comprobante c
+                WHERE c.importe <> (
+                    SELECT SUM(l.importe)
+                    FROM LineaComprobante l
+                    WHERE c.id_comp=l.id_comp AND c.id_tcomp = l.id_tcomp
+                    )
+            )
+    )*/
+
+CREATE OR REPLACE function tr_comprobante_sumaimportes() RETURNS trigger AS $$
+BEGIN
+    IF (EXISTS (SELECT 1
+            FROM comprobante c
+            WHERE c.importe <> (SELECT SUM(l.importe)
+                                FROM lineacomprobante l
+                                WHERE c.id_comp=l.id_comp
+                                        AND c.id_tcomp=l.id_tcomp)
+       )) THEN
+        RAISE EXCEPTION 'trigger: tr_comproobante_sumaimportes activado'
+    END IF;
+END $$
+language 'plpgsql'; --No esta terminado
