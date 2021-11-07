b9eca7b7774be2cfed4b93b5fc97219c54f7d1bf
EugenioRomanelli <eugenioromanelli.er@gmail.com>
2021-11-07 17:29:22 -0300

    Punto 4)

---
+++ SERVICES.sql
@@ -0,0 +1,2 @@
+-- a. Proveer el mecanismo que crea más adecuado para que al ser invocado (una vez por mes), tome todos los servicios
+-- que son periódicos y genere la/s factura/s correspondiente/s. Indicar si se deben proveer parámetros adicionales para su generación. Explicar además cómo resolvería el tema de la invocación mensual (pero no lo implemente).
